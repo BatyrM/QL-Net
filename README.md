120dc495ebf48e5f624d21f00b49fff043eae3f7
Batyrbek Mukhatbekov <batyrbekmukhatbekov@gmail.com>
2020-08-12 13:27:00 +0600

    Updated construct_dict.py to make dictionary construction case-dependent. Updated README.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 to train the model run QLNet.ipynb in Colab
+
+There are 7 possible cases for quantization:
+
+Case number = 1: Implement quantization for layer 1 only
+
+Case number = 2: Implement quantization for layer 2 only
+
+Case number = 3: Implement quantization for both layer 1 and 2
+
+Case number = 4: Implement quantization for input layer only
+
+Case number = 5: Implement quantization for input layer + layer 1
+
+Case number = 6: Implement quantization for input layer + layer 2
+
+Case number = 7: Implement quantization for input layer + layer 1 + layer 2
+
+Inside the QLNet.ipynb there are 3 different parts:
+
+1) Train baseline model.
+2) Construct dictionary for a lookup table: 
+In file training_parameters.py there is a parsing argument called case_number. This parameter is needed to distinguish between many cases as written above. Dependending on each case, dictionary is constructed only for needed layers. For ex., if case_number = 1, then dictionary is constructed only for layer 1. If case_number = 3, then dictionary is constructed for both layers 1 and 2.
+3) Train quantized model. Use case_number parsing argument to choose one of the cases from above. Depending on that number the layers you want to approximate will be successfully quantized.
--- construct_dict.py
+++ construct_dict.py
@@ -29,98 +29,266 @@ model.to(device).eval()
 case_number = args.case_number
 
 if case_number >= 4:
-    layer_id = 0
-    layer0 = 'layer' + str(layer_id)
-    activation_folder0 = os.path.join('./activations', layer0)
-    if_exist(activation_folder0)
+    if case_number == 4:
+        layer_id = 0
+        layer0 = 'layer' + str(layer_id)
+        activation_folder0 = os.path.join('./activations', layer0)
+        if_exist(activation_folder0)
+        ### 2. Load train data
+        train_loader = get_data(args, dataset='mnist', ifTrain=True)
+        ## 2. Extract activations for futher look-up dictionary construction
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            _, activations = model(data)
+            #print(len(activations))
+            activation0 = activations[0].cpu().data.numpy()
+            torch.save(activation0, os.path.join(activation_folder0, layer0 + '_'+str(batch_idx)+'.npy'))
+            if batch_idx>6:break
 
-    layer_id = 1
-    layer1 = 'layer' + str(layer_id)
-    activation_folder1 = os.path.join('./activations', layer1)
-    if_exist(activation_folder1)
+        # 3 Construct Look-up Dictionary
+        # parameters for look-up dictionary construction
+        n_cl = 10
+        max_depth = 1
 
-    layer_id = 2
-    layer2 = 'layer' + str(layer_id)
-    activation_folder2 = os.path.join('./activations', layer2)
-    if_exist(activation_folder2)
+        # Load activations
+        print('Load activations')
+        data0 = la.load_data(activation_folder0) # load patched data of input layer
+        print('Construct tree input layer')
+        tree0 = ht.construct(data0, n_cl, 1, max_depth)
+        torch.save(tree0, 'tree_' + layer0)
 
-    ### 2. Load train data
-    train_loader = get_data(args, dataset='mnist', ifTrain=True)
-    ## 2. Extract activations for futher look-up dictionary construction
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        _, activations = model(data)
-        #print(len(activations))
-        activation0 = activations[0].cpu().data.numpy()
-        activation1 = activations[1].cpu().data.numpy()
-        activation2 = activations[2].cpu().data.numpy()
+    elif case_number == 5:
+        layer_id = 0
+        layer0 = 'layer' + str(layer_id)
+        activation_folder0 = os.path.join('./activations', layer0)
+        if_exist(activation_folder0)
+
+        layer_id = 1
+        layer1 = 'layer' + str(layer_id)
+        activation_folder1 = os.path.join('./activations', layer1)
+        if_exist(activation_folder1)
+
+        ### 2. Load train data
+        train_loader = get_data(args, dataset='mnist', ifTrain=True)
+        ## 2. Extract activations for futher look-up dictionary construction
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            _, activations = model(data)
+            activation0 = activations[0].cpu().data.numpy()
+            activation1 = activations[1].cpu().data.numpy()
+            
+            torch.save(activation0, os.path.join(activation_folder0, layer0 + '_'+str(batch_idx)+'.npy'))
+            torch.save(activation1, os.path.join(activation_folder1, layer1 + '_'+str(batch_idx)+'.npy'))
+            if batch_idx>6:break
+
+        # 3 Construct Look-up Dictionary
+        # parameters for look-up dictionary construction
+        n_cl = 10
+        density = 30
+        max_depth = 1
+
+        # Load activations
+        print('Load activations')
+        data0 = la.load_data(activation_folder0) # load patched data of input layer
+        data1 = la.load_data(activation_folder1) # load patched data of layer1
+        print('Construct tree input layer')
+        tree0 = ht.construct(data0, n_cl, 1, max_depth)
+        torch.save(tree0, 'tree_' + layer0)
+        print('Construct tree layer 1')
+        tree1 = ht.construct(data1, n_cl, density, max_depth)
+        torch.save(tree1, 'tree_' + layer1)
+
+    elif case_number == 6:
+        layer_id = 0
+        layer0 = 'layer' + str(layer_id)
+        activation_folder0 = os.path.join('./activations', layer0)
+        if_exist(activation_folder0)        
         
-        torch.save(activation0, os.path.join(activation_folder0, layer0 + '_'+str(batch_idx)+'.npy'))
-        torch.save(activation1, os.path.join(activation_folder1, layer1 + '_'+str(batch_idx)+'.npy'))
-        torch.save(activation2, os.path.join(activation_folder2, layer2 + '_'+str(batch_idx)+'.npy'))
-        if batch_idx>6:break
+        layer_id = 2
+        layer2 = 'layer' + str(layer_id)
+        activation_folder2 = os.path.join('./activations', layer2)
+        if_exist(activation_folder2)
+        
+        ### 2. Load train data
+        train_loader = get_data(args, dataset='mnist', ifTrain=True)
+        ## 2. Extract activations for futher look-up dictionary construction
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            _, activations = model(data)
+            activation0 = activations[0].cpu().data.numpy()
+            activation2 = activations[2].cpu().data.numpy()
+            torch.save(activation0, os.path.join(activation_folder0, layer0 + '_'+str(batch_idx)+'.npy'))
+            torch.save(activation2, os.path.join(activation_folder2, layer2 + '_'+str(batch_idx)+'.npy'))
+            if batch_idx>6:break
 
-    # 3 Construct Look-up Dictionary
-    # parameters for look-up dictionary construction
-    n_cl = 10
-    density = 30
-    max_depth = 1
+        # 3 Construct Look-up Dictionary
+        # parameters for look-up dictionary construction
+        n_cl = 10
+        density = 30
+        max_depth = 1
 
-    # Load activations
-    print('Load activations')
-    data0 = la.load_data(activation_folder0) # load patched data of input layer
-    data1 = la.load_data(activation_folder1) # load patched data of layer1
-    data2 = la.load_data(activation_folder2) # load patched data of layer2
-    print('Construct tree input layer')
-    tree0 = ht.construct(data0, n_cl, 1, max_depth)
-    torch.save(tree0, 'tree_' + layer0)
-    print('Construct tree layer 1')
-    tree1 = ht.construct(data1, n_cl, density, max_depth)
-    torch.save(tree1, 'tree_' + layer1)
-    print('Construct tree layer 2')
-    tree2 = ht.construct(data2, n_cl, density, max_depth)
-    torch.save(tree2, 'tree_' + layer2)
+        # Load activations
+        print('Load activations')
+        data0 = la.load_data(activation_folder0) # load patched data of input layer
+        data2 = la.load_data(activation_folder2) # load patched data of layer2
+        print('Construct tree input layer')
+        tree0 = ht.construct(data0, n_cl, 1, max_depth)
+        torch.save(tree0, 'tree_' + layer0)
+        print('Construct tree layer 2')
+        tree2 = ht.construct(data2, n_cl, density, max_depth)
+        torch.save(tree2, 'tree_' + layer2)
+
+    elif case_number == 7:
+        layer_id = 0
+        layer0 = 'layer' + str(layer_id)
+        activation_folder0 = os.path.join('./activations', layer0)
+        if_exist(activation_folder0)
+
+        layer_id = 1
+        layer1 = 'layer' + str(layer_id)
+        activation_folder1 = os.path.join('./activations', layer1)
+        if_exist(activation_folder1)
+
+        layer_id = 2
+        layer2 = 'layer' + str(layer_id)
+        activation_folder2 = os.path.join('./activations', layer2)
+        if_exist(activation_folder2)
+
+        ### 2. Load train data
+        train_loader = get_data(args, dataset='mnist', ifTrain=True)
+        ## 2. Extract activations for futher look-up dictionary construction
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            _, activations = model(data)
+            #print(len(activations))
+            activation0 = activations[0].cpu().data.numpy()
+            activation1 = activations[1].cpu().data.numpy()
+            activation2 = activations[2].cpu().data.numpy()
+            
+            torch.save(activation0, os.path.join(activation_folder0, layer0 + '_'+str(batch_idx)+'.npy'))
+            torch.save(activation1, os.path.join(activation_folder1, layer1 + '_'+str(batch_idx)+'.npy'))
+            torch.save(activation2, os.path.join(activation_folder2, layer2 + '_'+str(batch_idx)+'.npy'))
+            if batch_idx>6:break
+
+        # 3 Construct Look-up Dictionary
+        # parameters for look-up dictionary construction
+        n_cl = 10
+        density = 30
+        max_depth = 1
+
+        # Load activations
+        print('Load activations')
+        data0 = la.load_data(activation_folder0) # load patched data of input layer
+        data1 = la.load_data(activation_folder1) # load patched data of layer1
+        data2 = la.load_data(activation_folder2) # load patched data of layer2
+        print('Construct tree input layer')
+        tree0 = ht.construct(data0, n_cl, 1, max_depth)
+        torch.save(tree0, 'tree_' + layer0)
+        print('Construct tree layer 1')
+        tree1 = ht.construct(data1, n_cl, density, max_depth)
+        torch.save(tree1, 'tree_' + layer1)
+        print('Construct tree layer 2')
+        tree2 = ht.construct(data2, n_cl, density, max_depth)
+        torch.save(tree2, 'tree_' + layer2)
 
 else:
-    layer_id = 1
-    layer1 = 'layer' + str(layer_id)
-    activation_folder1 = os.path.join('./activations', layer1)
-    if_exist(activation_folder1)
+    if case_number == 1:
+        layer_id = 1
+        layer1 = 'layer' + str(layer_id)
+        activation_folder1 = os.path.join('./activations', layer1)
+        if_exist(activation_folder1)
+        ### 2. Load train data
+        train_loader = get_data(args, dataset='mnist', ifTrain=True)
+        ## 2. Extract activations for futher look-up dictionary construction
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            _, activations = model(data)
+            activation1 = activations[1].cpu().data.numpy()
+            torch.save(activation1, os.path.join(activation_folder1, layer1 + '_'+str(batch_idx)+'.npy'))
+            if batch_idx>6:break
 
-    layer_id = 2
-    layer2 = 'layer' + str(layer_id)
-    activation_folder2 = os.path.join('./activations', layer2)
-    if_exist(activation_folder2)
+        # 3 Construct Look-up Dictionary
+        # parameters for look-up dictionary construction
+        n_cl = 10
+        density = 30
+        max_depth = 1
 
-    ### 2. Load train data
-    train_loader = get_data(args, dataset='mnist', ifTrain=True)
-    ## 2. Extract activations for futher look-up dictionary construction
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        _, activations = model(data)
-        #print(len(activations))
-        activation1 = activations[1].cpu().data.numpy()
-        activation2 = activations[2].cpu().data.numpy()
+        # Load activations
+        print('Load activations')
+        data1 = la.load_data(activation_folder1) # load patched data of layer1
+
+        print('Construct tree 1')
+        tree1 = ht.construct(data1, n_cl, density, max_depth)
+        torch.save(tree1, 'tree_' + layer1)
         
-        torch.save(activation1, os.path.join(activation_folder1, layer1 + '_'+str(batch_idx)+'.npy'))
-        torch.save(activation2, os.path.join(activation_folder2, layer2 + '_'+str(batch_idx)+'.npy'))
-        if batch_idx>6:break
+    elif case_number == 2:
+        layer_id = 2
+        layer2 = 'layer' + str(layer_id)
+        activation_folder2 = os.path.join('./activations', layer2)
+        if_exist(activation_folder2)
+        ### 2. Load train data
+        train_loader = get_data(args, dataset='mnist', ifTrain=True)
+        ## 2. Extract activations for futher look-up dictionary construction
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            _, activations = model(data)
+            activation2 = activations[2].cpu().data.numpy()
+            torch.save(activation2, os.path.join(activation_folder2, layer2 + '_'+str(batch_idx)+'.npy'))
+            if batch_idx>6:break
 
-    # 3 Construct Look-up Dictionary
-    # parameters for look-up dictionary construction
-    n_cl = 10
-    density = 30
-    max_depth = 1
+        # 3 Construct Look-up Dictionary
+        # parameters for look-up dictionary construction
+        n_cl = 10
+        density = 30
+        max_depth = 1
 
-    # Load activations
-    print('Load activations')
-    data1 = la.load_data(activation_folder1) # load patched data of layer1
-    data2 = la.load_data(activation_folder2) # load patched data of layer2
+        # Load activations
+        print('Load activations')
+        data2 = la.load_data(activation_folder2) # load patched data of layer2
 
-    print('Construct tree 1')
-    tree1 = ht.construct(data1, n_cl, density, max_depth)
-    torch.save(tree1, 'tree_' + layer1)
-    print('Construct tree 2')
-    tree2 = ht.construct(data2, n_cl, density, max_depth)
-    torch.save(tree2, 'tree_' + layer2)
+        print('Construct tree 2')
+        tree2 = ht.construct(data2, n_cl, density, max_depth)
+        torch.save(tree2, 'tree_' + layer2)
+    
+    elif case_number == 3:
+        layer_id = 1
+        layer1 = 'layer' + str(layer_id)
+        activation_folder1 = os.path.join('./activations', layer1)
+        if_exist(activation_folder1)
+
+        layer_id = 2
+        layer2 = 'layer' + str(layer_id)
+        activation_folder2 = os.path.join('./activations', layer2)
+        if_exist(activation_folder2)
+
+        ### 2. Load train data
+        train_loader = get_data(args, dataset='mnist', ifTrain=True)
+        ## 2. Extract activations for futher look-up dictionary construction
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = data.to(device), target.to(device)
+            _, activations = model(data)
+            activation1 = activations[1].cpu().data.numpy()
+            activation2 = activations[2].cpu().data.numpy()
+            
+            torch.save(activation1, os.path.join(activation_folder1, layer1 + '_'+str(batch_idx)+'.npy'))
+            torch.save(activation2, os.path.join(activation_folder2, layer2 + '_'+str(batch_idx)+'.npy'))
+            if batch_idx>6:break
+
+        # 3 Construct Look-up Dictionary
+        # parameters for look-up dictionary construction
+        n_cl = 10
+        density = 30
+        max_depth = 1
+
+        # Load activations
+        print('Load activations')
+        data1 = la.load_data(activation_folder1) # load patched data of layer1
+        data2 = la.load_data(activation_folder2) # load patched data of layer2
+
+        print('Construct tree 1')
+        tree1 = ht.construct(data1, n_cl, density, max_depth)
+        torch.save(tree1, 'tree_' + layer1)
+        print('Construct tree 2')
+        tree2 = ht.construct(data2, n_cl, density, max_depth)
+        torch.save(tree2, 'tree_' + layer2)
 
