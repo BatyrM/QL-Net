6d379ea5923962b9cf99900727e25bdc3d047e86
Batyrbek Mukhatbekov <batyrbekmukhatbekov@gmail.com>
2020-08-21 12:26:21 +0600

    Update vgg_quantized.py

--- vgg_quantized.py
+++ vgg_quantized.py
@@ -5,6 +5,7 @@ import math
 
 import torch.nn as nn
 import torch.nn.init as init
+import torch.nn.functional as F
 
 __all__ = [
     'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
@@ -18,7 +19,23 @@ class VGG(nn.Module):
     '''
     def __init__(self, features):
         super(VGG, self).__init__()
-        self.features = features
+        self.conv1 = nn.Conv2d(3, 64, kernel_size = 3, padding = 1)             'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M']
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size = 3, padding = 1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size = 3, padding = 1)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(256, 256, kernel_size = 3, padding = 1)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.conv5 = nn.Conv2d(256, 512, kernel_size = 3, padding = 1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.conv6 = nn.Conv2d(512, 512, kernel_size = 3, padding = 1)
+        self.bn6 = nn.BatchNorm2d(512)
+        self.conv7 = nn.Conv2d(512, 512, kernel_size = 3, padding = 1)
+        self.bn7 = nn.BatchNorm2d(512)
+        self.conv8 = nn.Conv2d(512, 512, kernel_size = 3, padding = 1)
+        self.bn8 = nn.BatchNorm2d(512)
+        self.activation = nn.ReLU(inplace = True)
         self.classifier = nn.Sequential(
             nn.Dropout(),
             nn.Linear(512, 512),
@@ -37,7 +54,13 @@ class VGG(nn.Module):
 
 
     def forward(self, x, n = 0, tree = None):
+        
+        
+        layer1 = self.activation(F.max_pool2d(self.bn1(self.conv1(x))))
+        
         out = self.features(x)
+        print(out.shape)
+        exit(0)
         out = out.view(out.size(0), -1)
         out = self.classifier(out)
         return out, x 
