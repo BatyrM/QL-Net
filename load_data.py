e3d4262d13ce1b05053341588a9f907c4e655e6b
Batyrbek Mukhatbekov <batyrbekmukhatbekov@gmail.com>
2020-08-23 03:14:20 +0600

    Small updates to load_data and vgg_quantized py files

--- load_data.py
+++ load_data.py
@@ -33,7 +33,7 @@ def get_cifar10(args, ifTrain):
     
     loader = None
     
-    kwargs = {'num_workers': args.workers, 'pin_memory': True} if args.cuda else {}
+    kwargs = {'num_workers': 4, 'pin_memory': True} if torch.cuda.is_available() else {}
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
--- vgg_quantized.py
+++ vgg_quantized.py
@@ -6,6 +6,7 @@ import math
 import torch.nn as nn
 import torch.nn.init as init
 import torch.nn.functional as F
+from quantizer_gpu import Quantizer
 
 __all__ = [
     'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
